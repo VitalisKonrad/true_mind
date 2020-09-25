bc7c6df7f10c84be01c8e2308f0c25aab9cc62b1
VitalisKonrad <46555488+VitalisKonrad@users.noreply.github.com>
2020-09-25 16:45:56 +0300

    before add usertext

--- true_mind_project/true_mind/settings.py
+++ true_mind_project/true_mind/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'true_mind.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -127,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
